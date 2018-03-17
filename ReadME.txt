902198017de43f2af871610f58273684010ef984
BartekStolarek <33466473+BartekStolarek@users.noreply.github.com>
2018-03-17 12:29:12 +0100

    Add files via upload

---
+++ ReadME.txt
@@ -0,0 +1,7 @@
+Technologies used in this project:
+- VueJS
+- jQuery
+- Google Fonts (Quicksand)
+- FontAwesome CSS
+- CountryFlags REST API (countryflags.io)
+- Universities API (universities.hipolabs.com)
---
+++ script.js
@@ -0,0 +1,34 @@
+var app = new Vue({
+  el: '#center',
+  data: {
+    name: '',
+    country: '',
+    universities : [],
+    countries: '',
+    dataToLoad: 26,
+    scrolled: false
+  },
+  methods: {
+    getData: function() {
+      var self = this;
+      $.get('http://universities.hipolabs.com/search?name=' + this.name + '&&country=' + this.country, function(data) {
+        self.universities = JSON.parse(data);
+      });
+      this.dataToLoad = 26;
+    },
+    getFlagUrl: function(countryCode) {
+      return "http://www.countryflags.io/" + countryCode + "/flat/64.png";
+    },
+    handleScroll () {
+      if (window.scrollY >= $("#results").height()-300) {
+        this.dataToLoad += 27;
+      }
+    }
+  },
+  created: function() {
+      window.addEventListener('scroll', this.handleScroll);
+    },
+  destroyed: function() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+});
